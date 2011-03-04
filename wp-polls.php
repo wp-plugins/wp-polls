e80c0f5b5066c320a926882ef10f4c42954cfe92
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-04 01:22:36 +0000

    Check whether WP-Stats exist after WP is loaded
    git-svn-id: https://plugins.svn.wordpress.org/wp-polls/trunk@355075 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-polls.php
+++ wp-polls.php
@@ -1347,9 +1347,14 @@ function vote_poll() {
 
 
 ### Function: Plug Into WP-Stats
-if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php') || strpos($_SERVER['REQUEST_URI'], 'wp-stats/wp-stats.php')) {
-	add_filter('wp_stats_page_admin_plugins', 'polls_page_admin_general_stats');
-	add_filter('wp_stats_page_plugins', 'polls_page_general_stats');
+add_action('wp','polls_wp_stats');
+function polls_wp_stats() {
+	if(function_exists('stats_page')) {
+		if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php') || strpos($_SERVER['REQUEST_URI'], 'wp-stats/wp-stats.php')) {
+			add_filter('wp_stats_page_admin_plugins', 'polls_page_admin_general_stats');
+			add_filter('wp_stats_page_plugins', 'polls_page_general_stats');
+		}
+	}
 }
 
 
